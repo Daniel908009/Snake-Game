e7873fc0ddc12d7b54c0a61bc2413cd5c233987e
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-07-20 14:49:05 +0200

    Final version of the code, fully resizable and fully working settings window

--- main.py
+++ main.py
@@ -4,45 +4,121 @@ import random
 import tkinter
 
 # function to apply the settings will be enhanced later
-def apply_settings(window):
+def apply_settings(window, food_number, resizability, speed):
+    global num_of_food, screen, frame_rate, end_screen
+    # checking if the input is correct
+    if int(food_number)  <= 0 or int(speed) <= 0:
+        return
+    # closing the settings window
     window.destroy()
+    end_screen = False
+    # changing the speed of the snake based on the input
+    frame_rate = int(speed)
+    # changing the number of food based on the input
+    num_of_food = int(food_number)
+    # changing the number of food on the screen based on the input
+    food.clear()
+    for i in range(num_of_food):
+        food.append(Food(random.choice(row), random.choice(column)))
+    # changing the resizability of the window based on the input
+    if resizability:
+        screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+    else:
+        screen = pygame.display.set_mode((800, 600))
     reset()
 
 # function for the settings window
 def settings_window():
     window = tkinter.Tk()
     window.title("Settings")
-    window.geometry("300x300")
+    window.geometry("600x300")
     window.resizable(False, False)
     window.iconbitmap("setting.ico")
     # creating a main label for the settings window
     main_label = tkinter.Label(window, text="Settings", font=("Arial", 20))
     main_label.pack(side="top")
+    # creating a frame for the settings
+    frame = tkinter.Frame(window)
+    frame.pack(side="top")
+    # creating a label for the number of food
+    food_label = tkinter.Label(frame, text="Number of food:")
+    food_label.grid(row=0, column=0)
+    # creating a entry for the number of food
+    e1 = tkinter.StringVar()
+    e1.set(str(num_of_food))
+    food_entry = tkinter.Entry(frame, textvariable=e1)
+    food_entry.grid(row=0, column=1)
+    # instructions for the user
+    instructions_label = tkinter.Label(frame, text="*how many will be on the screen at the same time", fg="red")
+    instructions_label.grid(row=0, column=2)
+    # creating a label for the resizability of the window
+    resizability_label = tkinter.Label(frame, text="Resizability:")
+    resizability_label.grid(row=1, column=0)
+    # creating a checkbutton for the resizability of the window
+    resizability_var = tkinter.IntVar()
+    resizability_checkbox = tkinter.Checkbutton(frame, variable=resizability_var)
+    resizability_checkbox.grid(row=1, column=1)
+    # instructions for the user
+    instructions_label1 = tkinter.Label(frame, text="*if checked, the window will be resizable", fg="red")
+    instructions_label1.grid(row=1, column=2)
+    # creating a label for the speed of the snake
+    speed_label = tkinter.Label(frame, text="Speed:")
+    speed_label.grid(row=2, column=0)
+    # creating a entry for the speed of the snake
+    e2 = tkinter.StringVar()
+    e2.set(str(frame_rate))
+    speed_entry = tkinter.Entry(frame, textvariable=e2)
+    speed_entry.grid(row=2, column=1)
+    # instructions for the user
+    instructions_label2 = tkinter.Label(frame, text="*Recommended speed is 6", fg="red")
+    instructions_label2.grid(row=2, column=2)
 
     # creating apply button
-    apply_button = tkinter.Button(window, text="Apply", width=10, height=2, command= lambda: apply_settings(window))
+    apply_button = tkinter.Button(window, text="Apply", width=10, height=2, command= lambda: apply_settings(window, food_entry.get(), resizability_var.get(), speed_entry.get()))
     apply_button.pack(side="bottom")
     window.mainloop()
 
 # function to reset the game
 def reset():
-    global snake, food, current_direction, score
+    global snake, food, current_direction, score, row, column, pixel_size, num_of_food
+    global width, height
+    # getting the width and height of the screen
+    width = screen.get_width()
+    height = screen.get_height()
+    # resetting the row and column lists
+    row.clear()
+    column.clear()
+    # getting the new pixel size
+    pixel_size = width//40
+    # getting all the possible positions for the apple and the snake with the new pixel size
+    for i in range(width//pixel_size):
+        row.append(i*pixel_size)
+    for i in range(height//pixel_size):
+        column.append(i*pixel_size)
+    # resetting the food and snake
+    food.clear()
+    for i in range(num_of_food):
+        food.append(Food(random.choice(row), random.choice(column)))
     snake = Snake(random.choice(row), random.choice(column), pixel_size)
-    food = Food(random.choice(row), random.choice(column))
+    # resetting other variables
     current_direction = ""
     score = 0
 
 # function for the game over screen
 def game_over_screen():
-    global running
+    global running, end_screen
     end_screen = True
     # keeping the screen running in case player wants to restart the game or edit the settings
     while end_screen:
         screen.fill((0, 0, 0))
         font = pygame.font.Font(None, 36)
+        # displaying the game over message
         text = font.render(f"Game Over! Your score is: {score}", True, (255, 255, 255))
-        screen.blit(text, (800//2-text.get_width()//2, 600//2-text.get_height()//2))
+        screen.blit(text, (width//2-text.get_width()//2, height//2-text.get_height()//2))
+        settings_button = pygame.image.load("setting.png")
+        screen.blit(settings_button, (width-settings_button.get_width()-10, 10))
         pygame.display.update()
+        # event loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 end_screen = False
@@ -54,14 +130,14 @@ def game_over_screen():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     mouse_x, mouse_y = pygame.mouse.get_pos()
-                    if mouse_x > 800//2-text.get_width()//2 and mouse_x < 800//2+text.get_width()//2 and mouse_y > 600//2-text.get_height()//2 and mouse_y < 600//2+text.get_height()//2:
+                    if mouse_x > width-settings_button.get_width()-10 and mouse_x < width-10 and mouse_y > 10 and mouse_y < 10+settings_button.get_height():
                         settings_window()
-                        end_screen = False
 
 # initialize pygame
 pygame.init()
 # setting up the screen
-screen = pygame.display.set_mode((800, 600))
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
 # setting up the title and icon
 pygame.display.set_caption("Snake Game")
 icon = pygame.image.load("snake.png")
@@ -91,20 +167,28 @@ class Food:
         pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, pixel_size, pixel_size))
 
 # variables
-pixel_size = 30
+pixel_size = width//40
 row = []
 column = []
+num_of_food = 1
 # getting all the possible positions for the apple and the snake
-for i in range(800//pixel_size):
+for i in range(width//pixel_size):
     row.append(i*pixel_size)
-for i in range(600//pixel_size):
+for i in range(height//pixel_size):
     column.append(i*pixel_size)
 
+# getting the starting position of the snake and the foods
 snake = Snake(random.choice(row), random.choice(column), pixel_size)
-food = Food(random.choice(row), random.choice(column))
+food = []
+for i in range(num_of_food):
+    food.append(Food(random.choice(row), random.choice(column)))
+
+# creating other variables
 current_direction = ""
 score = 0
 pygame.clock = pygame.time.Clock()
+frame_rate = 6
+end_screen = False
 
 # main game loop
 running = True
@@ -118,27 +202,33 @@ while running:
     screen.blit(text, (10, 10))
 
     # drawing the settings button
-    screen.blit(settings_button, (800-settings_button.get_width()-10, 10))
+    screen.blit(settings_button, (width-settings_button.get_width()-10, 10))
 
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
+            # checking and changing the direction of the snake based on the key pressed
             if event.key == pygame.K_UP or event.key == pygame.K_w:
-                current_direction = "up"
+                if current_direction != "down":
+                    current_direction = "up"
             if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                current_direction = "down"
+                if current_direction != "up":
+                    current_direction = "down"
             if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                current_direction = "left"
+                if current_direction != "right":
+                    current_direction = "left"
             if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                current_direction = "right"
+                if current_direction != "left":
+                    current_direction = "right"
             if event.key == pygame.K_r or event.key == pygame.K_RETURN:
                 reset()
+        # checking if the settings button is clicked
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                if mouse_x > 800-settings_button.get_width()-10 and mouse_x < 800-10 and mouse_y > 10 and mouse_y < 10+settings_button.get_height():
+                if mouse_x > width-settings_button.get_width()-10 and mouse_x < width-10 and mouse_y > 10 and mouse_y < 10+settings_button.get_height():
                     settings_window()
 
     # moving the snake based on the current direction
@@ -152,31 +242,34 @@ while running:
         snake.x += snake.speed
 
     # checking if the snake hit a wall
-    if snake.x < 0 or snake.x > 800 or snake.y < 0 or snake.y > 600:
+    if snake.x < 0 or snake.x > width or snake.y < 0 or snake.y > height:
         game_over_screen()
     
-    # checking if the snake ate the food
-    if snake.x == food.x and snake.y == food.y:
-        food = Food(random.choice(row), random.choice(column))
-        score += 1
+    # checking if the snake hit itself
+    for i in snake.body:
+        if snake.body.count(i) > 1:
+            game_over_screen()
+    
+    # checking if the snake ate at least one food
+    #print(food)
+    for i in food:
+        if snake.x == i.x and snake.y == i.y:
+            food.remove(i)
+            food.append(Food(random.choice(row), random.choice(column)))
+            score += 1
 
-    # drawing the food
-    food.draw()
+    # drawing the foods
+    for i in food:
+        i.draw()
 
-    # drawing the snake(new version of the code)
+    # drawing the snake and updating the body
     snake.body.append((snake.x, snake.y))
     if len(snake.body) > score+1:
         snake.body.pop(0)
     snake.draw()
 
-         # original code
-    #if len(snake.body) > score+1:
-    #    snake.body.pop(0)
-    #for i in snake.body:
-    #    pygame.draw.rect(screen, (0, 255, 0), (i[0], i[1], pixel_size, pixel_size))
-
     # setting the frame rate
-    pygame.clock.tick(6)
+    pygame.clock.tick(frame_rate)
 
     # update the screen
     pygame.display.update()
